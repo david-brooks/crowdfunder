921bab019e9581e364cdb4176f27a46e8e2fc73d
jeffquach <jquach8@gmail.com>
2014-02-25 20:36:30 -0500

    Tweaked associaiton

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-	belongs_to :owner, class_name: "User"
+	belongs_to :owner, foreign_key: "owner_id"
 	has_many :rewards
 	has_many :user_pledges, through: :pledges, source: :user
 	validates :title, presence: true, length: {minimum: 10}
