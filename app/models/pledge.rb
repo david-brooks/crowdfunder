3a655c34f0431a052e4fb83172ce90a9d0fb47b5
jeffquach <jquach8@gmail.com>
2014-02-26 17:16:42 -0500

    Added form for pledges

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,4 +2,10 @@ class Pledge < ActiveRecord::Base
 	belongs_to :project
 	belongs_to :user 
 	belongs_to :reward
+
+	validates :reward_id, presence: true
+
+	def reward_amount
+		project.rewards.each{|r| r.amount}
+	end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,11 @@
 class Project < ActiveRecord::Base
 	belongs_to :owner, class_name: "User"
 	has_many :rewards
-	has_many :pledges
+	has_many :pledges, through: :rewards
 	has_many :backers, through: :pledges, source: :user
 	validates :title, presence: true, length: {minimum: 10}
+
+	def amount_raised
+		"$#{rewards.sum(:amount)}"
+	end
 end
