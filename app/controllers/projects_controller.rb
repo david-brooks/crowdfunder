bde2e8d3de867988dbf277f93d407cdc0c8ec569
jeffquach <jquach8@gmail.com>
2014-02-27 13:53:50 -0500

    Got carrierwave working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -47,6 +47,6 @@ class ProjectsController < ApplicationController
 
 	private
 	def project_params
-		params.require(:project).permit(:title,:description,:goal,:start_date,:end_date)
+		params.require(:project).permit(:title,:description,:goal,:start_date,:end_date,:image)
 	end
 end
