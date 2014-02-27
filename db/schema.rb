8a6c39986d00fe7e5fa99bde9c717cde1ae260fd
David Brooks <davidbrooks.now@gmail.com>
2014-02-27 02:18:06 -0500

    integrated image slider

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,11 @@ ActiveRecord::Schema.define(version: 20140227000238) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_token"
-    t.boolean  "admin",           default: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.boolean  "admin",               default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
